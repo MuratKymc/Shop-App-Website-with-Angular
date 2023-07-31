10a093ec5c5d983941f3f25f186bf7f6369df8ae
Murat <murat.kymc.16@gmail.com>
2023-07-31 17:27:47 +0300

    11:sayfalama linkelri

--- src/app/shop/shop.component.ts
+++ src/app/shop/shop.component.ts
@@ -6,41 +6,56 @@ import { Category } from "../model/category.model";
 
 @Component({
     selector: 'shop',
-    templateUrl:'shop.component.html',
+    templateUrl: 'shop.component.html',
     styles: [`
         .pt-100 {padding-top:100px;}
     `]
 })
 
-export class ShopComponent{
+export class ShopComponent {
 
-    public selectedCategory: Category= null;
+    public selectedCategory: Category = null;
     public productPerPage = 3;
-    public selectedPage =2;
+    public selectedPage = 1;
+
 
     constructor(
         private productRepository: ProductRepository,
         private categoryRepository: CategoryRepository
-    ) {}
+    ) { }
 
 
-    get products(): Product[]{
-        let index = (this.selectedPage - 1 ) * this.productPerPage;
+
+    get products(): Product[] {
+        let index = (this.selectedPage - 1) * this.productPerPage;
 
 
         return this.productRepository
-                .getProducts(this.selectedCategory)
-                .slice(index, index + this.productPerPage);
+            .getProducts(this.selectedCategory)
+            .slice(index, index + this.productPerPage);
 
 
     }
 
+    get pageNumbers(): number[] {
+        return Array(Math.ceil(this.productRepository
+            .getProducts(this.selectedCategory).length / this.productPerPage))
+            .fill(0)
+            .map((a, i) => i + 1)
+    }
 
-    get categories(): Category[]{
+
+
+    changePage(p: number) {
+        this.selectedPage = p;
+    }
+
+
+    get categories(): Category[] {
         return this.categoryRepository.getCategories();
     }
 
-    changeCategory(newCategory?: Category){
+    changeCategory(newCategory?: Category) {
         this.selectedCategory = newCategory;
     }
 

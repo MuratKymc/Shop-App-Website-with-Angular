c668a10fa9bc45283c84bb0b5e8bc1e9cb43d8b3
Murat <murat.kymc.16@gmail.com>
2023-07-31 17:51:58 +0300

    12:add cart

--- src/app/shop/cart-summary/cart-summary.component.ts
+++ src/app/shop/cart-summary/cart-summary.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Cart } from 'src/app/model/cart.model';
 
 @Component({
   selector: 'cart-summary',
   templateUrl: './cart-summary.component.html',
   styleUrls: ['./cart-summary.component.css']
 })
-export class CartSummaryComponent {
+export class CartSummaryComponent implements OnInit {
+  
+  
+  constructor(public cart: Cart){}
+  
+  ngOnInit() {
+  }
 
 }

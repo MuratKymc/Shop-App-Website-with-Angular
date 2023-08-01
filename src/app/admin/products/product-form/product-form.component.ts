94ba4d527f705111c05bfbda09362a8bc553c9b6
Murat <murat.kymc.16@gmail.com>
2023-08-01 18:43:38 +0300

    30product-edit-form

--- src/app/admin/products/product-form/product-form.component.ts
+++ src/app/admin/products/product-form/product-form.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Product } from 'src/app/model/product.model';
+import { ProductRepository } from 'src/app/model/product.repository';
 
 @Component({
   selector: 'app-product-form',
   templateUrl: './product-form.component.html',
   styleUrls: ['./product-form.component.css']
 })
-export class ProductFormComponent {
+export class ProductFormComponent implements OnInit {
+ 
+  editing: boolean = false;
+  product: Product = new Product();
+
+  constructor(private activeRoute: ActivatedRoute, private repository: ProductRepository){
+    this.editing = activeRoute.snapshot.params['mode'] == 'edit';
+    if(this.editing){
+      this.product = repository.getProduct(activeRoute.snapshot.params['id']);
+    }
+  }
+
+
+ ngOnInit() {
+  }
 
 }

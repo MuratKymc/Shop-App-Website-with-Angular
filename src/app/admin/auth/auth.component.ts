cfa69df08f77767e7606e40d2e9943879d25a353
Murat <murat.kymc.16@gmail.com>
2023-08-01 15:09:34 +0300

    23login page added

--- src/app/admin/auth/auth.component.ts
+++ src/app/admin/auth/auth.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-auth',
   templateUrl: './auth.component.html',
   styleUrls: ['./auth.component.css']
 })
-export class AuthComponent {
+export class AuthComponent implements OnInit {
+
+  public username: string;
+  public password: string;
+  public errorMessage: string;
+
+
+  constructor(private router: Router) {
+
+  }
+
+  ngOnInit() {
+  }
+
+  login(form: NgForm) {
+
+    if (form.valid) {
+      if (this.username == 'admin' && this.password === '12345') {
+        this.router.navigateByUrl('/admin/main')
+      } else {
+        this.errorMessage = 'Hatalı isim ya da parola'
+      }
+    } else {
+      this.errorMessage = 'Bilgileri eksiksiz giriniz'
+    }
+
+
+
+  }
 
 }

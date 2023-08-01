b00a1624499227bab91f426a5f00235a57284431
Murat <murat.kymc.16@gmail.com>
2023-08-01 18:11:21 +0300

    29:admin product list

---
+++ src/app/admin/products/product-list/product-list.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { Product } from 'src/app/model/product.model';
+import { ProductRepository } from 'src/app/model/product.repository';
+
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css']
+})
+export class ProductListComponent implements OnInit {
+
+
+  constructor(private productRepository: ProductRepository) { }
+
+  ngOnInit() {
+
+  }
+
+
+  getProducts(): Product[] {
+    return this.productRepository.getProducts();
+  }
+
+
+}
